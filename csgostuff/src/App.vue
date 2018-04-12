e0e56f7bf70aa9eca5d89d20b8398312b3ab005c
Unknown <baptiste.viala@outlook.com>
2018-04-12 17:32:52 -0300

    fixed toolbar and footer

--- csgostuff/src/App.vue
+++ csgostuff/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    <v-toolbar dark>
+    <v-toolbar dark fixed>
       <v-toolbar-title class="mx-auto">CSGO Stuff</v-toolbar-title>
     </v-toolbar>
-    <v-content>
+    <v-content class="mt-5" >
       <router-view/>
     </v-content>
-    <v-footer class="pa-4">
+    <v-footer fixed class="pa-2">
       <div class="mx-auto">WORK IN PROGRESS</div>
     </v-footer>
   </v-app>
--- csgostuff/src/components/StuffItem.vue
+++ csgostuff/src/components/StuffItem.vue
@@ -2,14 +2,19 @@
     <v-flex>
         <v-card>
             <v-card-media :src="stuff.gifURL" height="300px"></v-card-media>
-            <v-card-title primary-title>
-                <h3 class="headline mb-0">
+            <v-card-title class="mb-0">
+                <h3 class="headline">
                      {{stuff.name}}
                 </h3>
             </v-card-title>
-            <v-card-actions>
-                <v-btn dark>See</v-btn>
-            </v-card-actions>
+            <v-card-text class="mt-0 pt-0">
+                <div>{{stuff.map}}</div> 
+                <div>{{stuff.stuffType}}</div>
+                <div>{{stuff.score}}</div>
+            </v-card-text>
+            <!-- <v-card-actions>
+                <v-btn flat>See more</v-btn>
+            </v-card-actions> -->
         </v-card>
     </v-flex>
 </template>
